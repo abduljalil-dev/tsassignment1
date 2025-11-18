4875e7ab0a9aa91869f09a86216f0e62649849c6
abduljalil-dev <freelancerjolil@gmail.com>
2025-11-18 22:32:27 +0600

    first commit

---
+++ solution.ts
@@ -0,0 +1,40 @@
+
+// Problem 1
+function formatValue(value: string | number | boolean): string | number | boolean {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+
+  else if (typeof value === "number") {
+    return value * 10;
+  }
+
+  else if (typeof value === "boolean") {
+    return !value;
+  }
+
+  return value;
+}
+
+
+// Problem 2
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  }
+
+  else if (Array.isArray(value)) {
+    return value.length;
+  }
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
